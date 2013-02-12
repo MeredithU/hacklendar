380e9d90b5589890c461feb182984653ca874682
Meredith Underell <mer.underell@gmail.com>
2013-02-12 00:32:19 -0500

    Working JavaScript

--- app/controllers/diary_entries_controller.rb
+++ app/controllers/diary_entries_controller.rb
@@ -7,17 +7,44 @@ class DiaryEntriesController < ApplicationController
 	end
 
 	def new
-		@diary_entries = DiaryEntry.new
+		@diary_entry = DiaryEntry.new
 	end
 
-	def create
-		@diary_entry = DiaryEntry.create params["diary_entry"]
 
-		if (DiaryEntry.title == "")
-			render new_diary_entry_path
-		else
+	def create
+		@diary_entry = DiaryEntry.new params[:diary_entry]
+
+		# the if statement validates whether it saved or not
+		if @diary_entry.save
 			redirect_to diary_entries_path
+		else
+			render :new
 		end
+	end
+
+
+	def edit
+		@diary_entry = DiaryEntry.find(params[:id])
+	end
+
+
+	def update
+		@diary_entry = DiaryEntry.find(params[:id])
+
+		if @diary_entry.update_attributes params[:diary_entry]
+			redirect_to diary_entries_path
+		else
+			render :edit
+		end
+	end
+
+
+	def destroy
+		@diary_entry = DiaryEntry.find(params[:id])
+
+		@diary.destroy
+
+		redirect_to diary_entries_path
 	end
 
 end
--- app/models/diary_entry.rb
+++ app/models/diary_entry.rb
@@ -1,15 +1,30 @@
 class DiaryEntry < ActiveRecord::Base
 	attr_accessible :title, :description, :mood
 
+	validates :title, :presence => true, :uniqueness => true
+  validates :description, :presence => true
+  validates :mood, :inclusion => { :in => %w(happy excited) }
+
+  validate :mondaythursday
+
+  #customized validation method
+  def mondaythursday
+    if Time.now.monday? || Time.now.thursday?
+      if !happy?
+        errors[:mood] << "must be happy."
+      end
+    end
+  end
+
 
 	def self.positive(title)
   		where(mood: title).limit(5)
-  	end
+  end
 
-  	# this is an instance method, in order to check against all of the diary entries,
-  	# pull out the specific id then check.
-  	def happy?
-  		mood == "Happy"
-  	end
+  # this is an instance method, in order to check against all of the diary entries,
+  # pull out the specific id then check.
+  def happy?
+  	mood == "happy"
+  end
 
 end
